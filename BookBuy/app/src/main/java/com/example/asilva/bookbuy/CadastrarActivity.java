79e7d19bc560286aa696a862062547175a122ccd
Augusto Lima da Silva <augusto_ls17@hotmail.com>
2015-08-12 17:46:49 -0300

    Tela de login, cadastro e recuperar senha feita.

---
+++ BookBuy/app/src/main/java/com/example/asilva/bookbuy/CadastrarActivity.java
@@ -0,0 +1,23 @@
+package com.example.asilva.bookbuy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CadastrarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_cadastrar, menu);
+        return true;
+    }
+}
