430a25ea2c61d84e01c9d4d0093387d429698270
Augusto Lima da Silva <augusto_ls17@hotmail.com>
2015-08-17 19:44:21 -0300

    Alteração em tela

--- BookBuy/app/src/main/java/com/example/asilva/bookbuy/MapaActivity.java
+++ BookBuy/app/src/main/java/com/example/asilva/bookbuy/MapaActivity.java
@@ -1,107 +1,227 @@
 package com.example.asilva.bookbuy;
 
-import android.content.Context;
-import android.content.IntentSender;
+import android.content.Intent;
 import android.location.Location;
-import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
+import com.mikepenz.materialdrawer.Drawer;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.SwitchDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.OnCheckedChangeListener;
 
 public class MapaActivity extends FragmentActivity implements
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GoogleMap mMap;
     double mLat;
     double mLong;
-    Location mLoc;
+    Location l;
     protected GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    LatLng latLng;
+    private AccountHeader.Result headerNavigationLeft;
+    private Drawer.Result navigationDrawerLeft;
+    Marker marker, mMarkerLocalAtual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
-        setUpMapIfNeeded();
-        buildGoogleApiClient();
+        //setUpMapIfNeeded();
+        callConnection();
 
-    }
+        //NAVIGATION DRAWER
 
-    protected  synchronized  void buildGoogleApiClient(){
-        mGoogleApiClient =  new  GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
+        headerNavigationLeft = new AccountHeader()
+                .withActivity(this)
+                .withCompactStyle(false)
+                .withSavedInstance(savedInstanceState)
+                .withThreeSmallProfileImages(true)
+                .withHeaderBackground(R.drawable.ic_perfil)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Person One").withEmail("person1@gmail.com").withIcon(getResources().getDrawable(R.drawable.ic_perfil))
+                )
                 .build();
+
+        navigationDrawerLeft = new Drawer()
+                .withActivity(this)
+                .withDisplayBelowToolbar(false)
+                .withActionBarDrawerToggleAnimated(true)
+                .withDrawerGravity(Gravity.LEFT)
+                .withSavedInstance(savedInstanceState)
+                .withSelectedItem(0)
+                .withActionBarDrawerToggle(true)
+                .withAccountHeader(headerNavigationLeft)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l, IDrawerItem iDrawerItem) {
+                        switch (i) {
+                            case 0:
+                                Intent it = new Intent(MapaActivity.this, MinhaContaActivity.class);
+                                startActivity(it);
+                                break;
+                            case 1:
+                                Intent ite = new Intent(MapaActivity.this, MinhasComprasActivity.class);
+                                startActivity(ite);
+                                break;
+                            case 2:
+                                Intent sharing = new Intent(Intent.ACTION_SEND);
+                                sharing.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+                                sharing.setType("text/plain");
+                                sharing.putExtra(Intent.EXTRA_TEXT, "Book Buy - Faça já sua reserva em um de nossos parceiros.");
+                                startActivity(Intent.createChooser(sharing, "Convide os amigos"));
+                                break;
+                        }
+                    }
+                })
+                .build();
+
+
+        navigationDrawerLeft.addItem(new PrimaryDrawerItem().withName("Perfil").withIcon(R.drawable.ic_minha_conta));
+        navigationDrawerLeft.addItem(new PrimaryDrawerItem().withName("Minhas compras").withIcon(R.drawable.ic_minhas_compras));
+        navigationDrawerLeft.addItem(new PrimaryDrawerItem().withName("Compartilhar").withIcon(R.drawable.ic_compartilhar));
+        navigationDrawerLeft.addItem(new SectionDrawerItem().withName("Configurações"));
+        navigationDrawerLeft.addItem(new SwitchDrawerItem().withName("Notificações").withChecked(true));
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        if(mGoogleApiClient !=null && mGoogleApiClient.isConnected()){
+            startLocationUpdate();
+        }
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
+    public void onPause(){
+        super.onPause();
+
+        if(mGoogleApiClient != null){
+            stopLocationUpdate();
+        }
+    }
+
+    private synchronized void callConnection(){
+        Log.i("LOG", "callConnection()");
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addOnConnectionFailedListener(this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
         mGoogleApiClient.connect();
     }
 
-    @Override
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
+
+    private void initLocationRequest(){
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(2000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+
+    private void startLocationUpdate(){
+        initLocationRequest();
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, MapaActivity.this);
+    }
+
+
+    private void stopLocationUpdate(){
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, MapaActivity.this);
     }
 
     @Override
-    public  void onConnected ( Bundle connectionHint ) {
-        mLoc = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient);
-        if (mLoc != null) {
-            mLat = mLoc.getLatitude();
-            mLong = mLoc.getLongitude();
+    public void onConnected(Bundle bundle) {
+        Log.i("LOG", "onConnected(" + bundle + ")");
+
+        l = LocationServices
+                .FusedLocationApi
+                .getLastLocation(mGoogleApiClient);
+
+        if(l != null){
+            mLat = l.getLatitude();
+            mLong = l.getLongitude();
+
+            latLng = new LatLng(mLat, mLong);
+            atualizarMapa();
         }
+
+        startLocationUpdate();
+    }
+
+    public void customAddMarker(LatLng latLng, String title){
+        MarkerOptions options = new MarkerOptions();
+        options.position(latLng).title(title).draggable(false);
+        options.icon(BitmapDescriptorFactory.fromResource(R.drawable.pin));
+
+        marker = mMap.addMarker(options);
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        Log.i("LOG", "onConnectionSuspended(" + i + ")");
     }
-
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-
+        Log.i("LOG", "onConnectionFailed(" + connectionResult + ")");
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setUpMapIfNeeded();
-    }
-
+/*
     private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.frag_maps))
                     .getMap();
-            // Check if we were successful in obtaining the map.
             if (mMap != null) {
-                setUpMap();
+                atualizarMapa();
             }
+        }
+    } */
+
+    private void atualizarMapa(){
+        mMap.clear();
+        if(latLng != null){
+            CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(13).tilt(0).build();
+            CameraUpdate update = CameraUpdateFactory.newCameraPosition(cameraPosition);
+            mMap.moveCamera(update);
+
+            customAddMarker(latLng, "Marcador 1");
         }
     }
 
-    private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(mLat, mLong)).title("Marker"));
+    @Override
+    public void onLocationChanged(Location location) {
+       // mMarkerLocalAtual.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
+        mMap.clear();
+        if(latLng != null){
+            CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(13).tilt(0).build();
+            CameraUpdate update = CameraUpdateFactory.newCameraPosition(cameraPosition);
+            mMap.moveCamera(update);
 
+            customAddMarker(latLng, "Marcador 1");
+        }
     }
-
-
 }
