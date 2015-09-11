09abc6f96bbb4ece9ed29a51176f59c6e7b59a41
Airynne Marques <airynne.marques@mobilicidade.com.br>
2015-09-11 20:52:57 -0300

    Cadastro de cliente - ultimate (mobile)

--- BookBuy/app/src/main/java/com/example/asilva/bookbuy/activities/CadastrarActivity.java
+++ BookBuy/app/src/main/java/com/example/asilva/bookbuy/activities/CadastrarActivity.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.asilva.bookbuy.R;
 import com.example.asilva.bookbuy.basicas.Cliente;
@@ -14,6 +16,12 @@ import com.example.asilva.bookbuy.dao.DAOCliente;
 public class CadastrarActivity extends AppCompatActivity {
 
     Button bttCadastrar;
+    EditText txtUsuario;
+    EditText txtNome;
+    EditText txtEmail;
+    EditText txtDDD;
+    EditText txtTelefone;
+    EditText txtSenha;
 
 
     @Override
@@ -21,6 +29,12 @@ public class CadastrarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar);
 
+
+        txtUsuario = (EditText)findViewById(R.id.txtUsuario);
+        txtNome = (EditText)findViewById(R.id.txtNome);
+        txtEmail = (EditText)findViewById(R.id.txtEmail);
+        txtTelefone = (EditText)findViewById(R.id.txtTelefone);
+        txtSenha = (EditText)findViewById(R.id.txtSenha);
         bttCadastrar = (Button)findViewById(R.id.bttCadastrar);
 
         bttCadastrar.setOnClickListener(new View.OnClickListener() {
@@ -28,12 +42,18 @@ public class CadastrarActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Cliente c = new Cliente();
 
-                c.setId(2);
-                c.setUsuario("teste");
-                c.setNome("teste");
-                c.setEmail("teste");
-                c.setTelefone("teste");
-                c.setSenha("teste");
+                c.setUsuario(txtUsuario.getText().toString());
+                c.setNome(txtNome.getText().toString());
+                c.setEmail(txtEmail.getText().toString());
+                c.setTelefone(txtTelefone.getText().toString());
+                c.setSenha(txtSenha.getText().toString());
+
+                //c.setId(2);
+                //c.setUsuario("teste");
+                //c.setNome("teste");
+                //c.setEmail("teste");
+                //c.setTelefone("teste");
+                //c.setSenha("teste");
 
                 DAOCliente clienteDAO = new DAOCliente();
                 boolean resultado = clienteDAO.inserirCliente(c);
